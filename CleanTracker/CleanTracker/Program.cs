93cf36320dd5283798038f5bf3cfbd07a8734b98
Lukas Szerszen <Lukassz@kth.se>
2021-05-01 16:25:51 +0200

    Initial commit

---
+++ CleanTracker/CleanTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CleanTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

7d8e730249deda25ce4e382ad91463b67f9403b9
Lukas Szerszen <Lukassz@kth.se>
2021-05-10 21:52:45 +0200

    wip: IssueCollection

---
+++ src/Domain/IssueCollection.cs
@@ -0,0 +1,32 @@
+﻿using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class IssueCollection : Dictionary<IssueId, IIssue>
+    {
+       
+        public IssuePoints GetTotalPoints()
+        {
+            if(this.Count == 0)
+            {
+                return new IssuePoints(0);
+            }
+
+            IssuePoints sum = new IssuePoints(0);
+
+            return this.Values.Aggregate(sum, (currentIssue, nextIssue) => new IssuePoints(sum.Points + currentIssue.Points));
+        }
+
+        public double CalculateKPI()
+        {
+            return this.Values.Average(issue => issue.Points.Points);
+        }
+
+       
+    }
+}
---
+++ test/UnitTests/Domain/UserTests.cs
@@ -0,0 +1,34 @@
+﻿using Domain;
+using Domain.ValueObjects;
+using System;
+using Xunit;
+
+namespace UnitTests.Domain
+{
+    public class UserTests
+    {
+        [Fact]
+        public void CalculateKPI_returns_the_averageKPi()
+        {
+            //Arrange
+            IssueCollection assignedIssues = new IssueCollection();
+            IssueId id = new IssueId(new Guid("9245fe4a-d402-451c-b9ed-9c1a04247482"));
+            IssueDescription description = new IssueDescription("This is an issue");
+            IssuePoints points = new IssuePoints(2);
+            string assignedTo = "asignee";
+            Issue i1 = new Issue(id, description, points, assignedTo);
+            assignedIssues.Add(i1.Id, i1);
+            id = new IssueId(new Guid("9245fe4a-d402-451c-b9ed-9c1a04247483"));
+            Issue i2 = new Issue(id, description, points, assignedTo);
+            assignedIssues.Add(i2.Id, i2);
+
+            //Act
+            double expectedAverage = 2;
+            double average = assignedIssues.CalculateKPI();
+
+            //Assert
+            Assert.Equal(expectedAverage, average);
+
+        }
+    }
+}

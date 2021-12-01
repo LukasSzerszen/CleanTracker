6bdf6dcc6e762087457a53527e1f2185288539bd
Lukas Szerszen <Lukassz@kth.se>
2021-12-01 21:03:50 +0100

    add: move issue use case

--- src/Application/UseCases/MoveIssue/MoveIssueUseCase.cs
+++ src/Application/UseCases/MoveIssue/MoveIssueUseCase.cs
@@ -1,14 +1,39 @@
-﻿using System;
+﻿using Domain;
+using Domain.Interfaces;
+using Domain.ValueObjects;
+using System;
 using System.Threading.Tasks;
 
 namespace Application.UseCases.MoveIssue;
 
 public class MoveIssueUseCase : IMoveIssueUseCase
 {
+    private readonly IIssueRepository _issueRepository;
+    private IOutputPort _outputPort;
 
-    public Task Execute(Guid IssueId)
+    public MoveIssueUseCase(IIssueRepository issueRepository)
     {
-        throw new NotImplementedException();
+        _issueRepository = issueRepository;
+        _outputPort = new MoveIssuePresenter();
     }
 
+    public void SetOutputPort(IOutputPort outputPort)
+    {
+        _outputPort = outputPort;
+    }
+
+
+    public Task Execute(Guid issueId, IssueProgressStatus issueProgressStauts)
+    {
+        var id = TrackerId.Build(issueId).Value;
+
+        return this.MoveIssue(id, issueProgressStauts);
+    }
+
+    private async Task MoveIssue(TrackerId issueId, IssueProgressStatus issueProgressStatus)
+    {
+        var issue = await _issueRepository.Get(issueId);
+        issue.UpdateProgress(issueProgressStatus);
+        return;
+    }
 }

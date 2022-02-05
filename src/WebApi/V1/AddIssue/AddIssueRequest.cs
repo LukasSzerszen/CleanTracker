72167cd0bb6ca5f1ff7b00d0c44daa9544e0a0a6
Lukas Szerszen <Lukassz@kth.se>
2022-02-05 14:14:31 +0100

    add: failing test

---
+++ src/WebApi/V1/AddIssue/AddIssueRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.V1.AddIssue;
+
+public sealed class AddIssueRequest
+{
+    [Required]
+    public Guid IssueId { get; set; }
+    [Required]
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public int? Points { get; set; }
+    public Guid? AssignedTo { get; set; }
+    public string? Status { get; set; }
+
+}
---
+++ test/ComponentTests/V1/AddIssueTests.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.V1.AddIssue;
+using Xunit;
+
+namespace ComponentTests.V1;
+
+public sealed class AddIssueTests : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly CustomWebApplicationFactory _fixture;
+    public AddIssueTests(CustomWebApplicationFactory fixture) => this._fixture = fixture;
+
+    [Fact]
+    public async Task AddIssueReturnsOk()
+    {
+        HttpClient client = _fixture.CreateClient();
+        var id = new Guid("22a69812-2d66-422f-90b8-d5a969b9f081");
+        Uri requestUri = new($"api/v1/Issue/addissue", UriKind.Relative);
+        var request = new AddIssueRequest()
+        {
+            IssueId = id,
+            Title = "new issue title",
+            Description = "issue description"
+        };
+
+        string json = JsonConvert.SerializeObject(request);
+        StringContent data = new StringContent(json, Encoding.UTF8, "application/json");
+
+        HttpResponseMessage actualResponse = await client.PostAsync(requestUri, data);
+
+        Assert.Equal(HttpStatusCode.OK, actualResponse.StatusCode);
+    }
+}

5ef57837ff07d628fd272596bf8719390ad0a87b
Lukas Szerszen <Lukassz@kth.se>
2022-04-02 13:19:47 +0200

    add: End to End tests

--- test/ComponentTests/CustomWebApplicationFactory.cs
+++ test/ComponentTests/CustomWebApplicationFactory.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 
 namespace ComponentTests;
 
 public sealed class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
+    public CustomWebApplicationFactory() => Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "ComponentTests");
     protected override void ConfigureWebHost(IWebHostBuilder builder) => builder.ConfigureAppConfiguration((context, config) =>
     {
         config.AddInMemoryCollection(
---
+++ test/EndToEndTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace EndToEndTests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    public CustomWebApplicationFactory() => Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "EndToEndTests");
+    protected override void ConfigureWebHost(IWebHostBuilder builder) => builder.ConfigureAppConfiguration((context, config) =>
+        {
+            config.AddInMemoryCollection(new Dictionary<string, string>
+            {
+                ["FeatureManagement:SqlServer"] = "true",
+                ["SqlServer:ConnectionString"] = "Server=localhost;User Id=sa;Password=Your_password123;Database=Issues;"
+
+            });
+        });
+}
